7e354fa5afe500bf5db22225ad6288549929e64f
mika <mail@deadcells.org>
2015-11-10 18:27:01 +0100

    User specific options UI work
    Worked on issue #9 to add the UI side of the user specific options

--- Source/options.js
+++ Source/options.js
@@ -7,11 +7,21 @@ function toggle_instructions() {
     }
 }
 
+function toggle_user_specific() {
+    var element = document.getElementById('user-specific-options');
+    if (element.style.display == 'block') {
+        element.style.display = 'none';
+    } else {
+        element.style.display = 'block';
+    }
+}
+
 function save_options() {
   var bgURL = document.getElementById('bgURL').value;
   var bgTile = document.getElementById('bgTile').checked;
   var bgColour = document.getElementById('bgColour').value;
   var bgType = document.getElementById('bgType').value;
+
   chrome.storage.sync.set({
     backgroundURL: bgURL,
     backgroundTile: bgTile,
@@ -39,7 +49,28 @@ function restore_options() {
     document.getElementById('bgType').value = items.backgroundType;
   });
 }
+
+function add_user_row() {
+    $('#user-options').append('<div class="user-option">\
+        @<input type="text" class="username"/>\
+        <input type="text" class="userimage"/>\
+        <input type="text" class="usercolour"/>\
+        <select class="userBgType">\
+            <option value="image">Image</option>\
+            <option value="colour">Colour</option>\
+        </select>\
+    </div>')
+}
+
+function remove_user_row() {
+    $('#user-options .user-option:last-child').remove();
+}
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
 document.getElementById('instruction-button').addEventListener('click', toggle_instructions);
+
+document.getElementById('user-specific-button').addEventListener('click', toggle_user_specific);
+document.getElementById('add').addEventListener('click', add_user_row);
+document.getElementById('remove').addEventListener('click', remove_user_row);
