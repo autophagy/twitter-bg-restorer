d4eee11837dddff5469d72716afe8f009476e8c9
mika <mail@deadcells.org>
2015-11-12 06:06:02 +0100

    Added proper saving and restoring of user options
    Further work on issue #9

--- Source/options.js
+++ Source/options.js
@@ -22,11 +22,26 @@ function save_options() {
   var bgColour = document.getElementById('bgColour').value;
   var bgType = document.getElementById('bgType').value;
 
+  var userBgOptions = new Array();
+
+  $('#user-options .user-option').each(function() {
+      username = $(this).find('input.username').val().trim();
+      if(username != '') {
+          userImage = $(this).find('input.userimage').val().trim();
+          userTile = $(this).find('input.usertile').is(':checked');
+          userColour = $(this).find('input.usercolour').val().trim();
+          userType = $(this).find('select.userbgtype').val().trim();
+
+          userBgOptions.push([username, userImage, userTile, userColour, userType]);
+      }
+  });
+
   chrome.storage.sync.set({
     backgroundURL: bgURL,
     backgroundTile: bgTile,
     backgroundColour: bgColour,
-    backgroundType: bgType
+    backgroundType: bgType,
+    userOptions: userBgOptions
   }, function() {
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
@@ -41,25 +56,43 @@ function restore_options() {
     backgroundURL: '',
     backgroundTile: true,
     backgroundColour: '',
-    backgroundType: 'image'
+    backgroundType: 'image',
+    userOptions: []
   }, function(items) {
     document.getElementById('bgURL').value = items.backgroundURL;
     document.getElementById('bgTile').checked = items.backgroundTile;
     document.getElementById('bgColour').value = items.backgroundColour;
     document.getElementById('bgType').value = items.backgroundType;
+
+    for (var i in items.userOptions) {
+        userOption = items.userOptions[i];
+        add_user_row(userOption[0], userOption[1], userOption[2], userOption[3], userOption[4]);
+    }
+
+    add_user_row('', '', false, '', 'image');
+
   });
 }
 
-function add_user_row() {
-    $('#user-options').append('<div class="user-option">\
+function add_user_row(username, image, tile, colour, type) {
+    var row = $('<div class="user-option">\
         @<input type="text" class="username"/>\
         <input type="text" class="userimage"/>\
+        <input type="checkbox" class="usertile">\
         <input type="text" class="usercolour"/>\
-        <select class="userBgType">\
+        <select class="userbgtype">\
             <option value="image">Image</option>\
             <option value="colour">Colour</option>\
         </select>\
-    </div>')
+    </div>');
+
+    row.find('input.username').val(username);
+    row.find('input.userimage').val(image);
+    row.find('input.usertile').prop('checked', tile);
+    row.find('input.usercolour').val(colour);
+    row.find('select.userbgtype').val(type);
+
+    $('#user-options').append(row);
 }
 
 function remove_user_row() {
@@ -72,5 +105,7 @@ document.getElementById('save').addEventListener('click',
 document.getElementById('instruction-button').addEventListener('click', toggle_instructions);
 
 document.getElementById('user-specific-button').addEventListener('click', toggle_user_specific);
-document.getElementById('add').addEventListener('click', add_user_row);
+document.getElementById('add').addEventListener('click', function(){
+    add_user_row('','',false,'','image');
+});
 document.getElementById('remove').addEventListener('click', remove_user_row);
