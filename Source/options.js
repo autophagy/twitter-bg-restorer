8c82f74899de9cd34f8fbf52c6be64691bd24396
Autophagy <mail@deadcells.org>
2016-08-17 17:58:06 +0200

    Fixed issue with non-saving if no new image selected

--- Source/options.js
+++ Source/options.js
@@ -8,6 +8,7 @@ function toggle_instructions() {
 }
 
 function save_options() {
+
   var bgTile = document.getElementById('bgTile').checked;
   var bgColour = document.getElementById('bgColour').value;
   var bgType = document.getElementById('bgType').value;
@@ -19,47 +20,62 @@ function save_options() {
 
   var input = document.getElementById('imageFile');
   file = input.files[0];
+
   if(file) {
-    fileReader = new FileReader();
-    fileReader.onload = function() {
-      var image = new Image();
-      image.onload = function() {
-        var canvas = document.createElement('CANVAS');
-        var context = canvas.getContext('2d');
-
-        canvas.height = this.height;
-        canvas.width = this.width;
-        context.drawImage(this, 0, 0);
-        var bgBase64 = canvas.toDataURL();
-        canvas = null;
-
-        chrome.storage.local.set({
-          backgroundBase64: bgBase64
-        }, function() { });
-
-        chrome.storage.sync.set({
-          backgroundURL: file.name,
-          backgroundTile: bgTile,
-          backgroundColour: bgColour,
-          backgroundType: bgType
-        }, function() {
-          var status = document.getElementById('status');
-          status.textContent = 'Options saved.';
-          restore_options();
-          setTimeout(function() {
-            status.textContent = '';
-          }, 750);
-        });
-      }
-      image.src = fileReader.result;
-    }
-    fileReader.readAsDataURL(file);
+    save_new_file(file, bgTile, bgColour, bgType);
   } else {
+    chrome.storage.local.get({
+      backgroundBase64: ''
+    }, function(localItems) {
+
+      if (!(localItems.backgroundBase64 == '' || localItems.backgroundBase64 == undefined)) {
+        set_chrome_storage(localItems.backgroundBase64, '', bgTile, bgColour, bgType);
+      } else {
+        set_chrome_storage('', '', bgTile, bgColour, bgType);
+      }
+    });
+  }
+}
+
+function save_new_file(file, bgTile, bgColour, bgType) {
+  fileReader = new FileReader();
+  fileReader.onload = function() {
+    var image = new Image();
+    image.onload = function() {
+      var canvas = document.createElement('CANVAS');
+      var context = canvas.getContext('2d');
+
+      canvas.height = this.height;
+      canvas.width = this.width;
+      context.drawImage(this, 0, 0);
+      var bgBase64 = canvas.toDataURL();
+      canvas = null;
+
+      set_chrome_storage(bgBase64, file.name, bgTile, bgColour, bgType);
+    }
+    image.src = fileReader.result;
+  }
+  fileReader.readAsDataURL(file);
+}
+
+function set_chrome_storage(bgBase64, bgURL, bgTile, bgColour, bgType) {
+  chrome.storage.local.set({
+    backgroundBase64: bgBase64
+  }, function() { });
+
+  chrome.storage.sync.set({
+    backgroundURL: bgURL,
+    backgroundTile: bgTile,
+    backgroundColour: bgColour,
+    backgroundType: bgType
+  }, function() {
+    var status = document.getElementById('status');
     status.textContent = 'Options saved.';
+    restore_options();
     setTimeout(function() {
       status.textContent = '';
     }, 750);
-  }
+  });
 }
 
 function restore_options() {
